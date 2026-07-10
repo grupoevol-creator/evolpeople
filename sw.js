a08115fbb48b0cd5b67378f6bf9fcefc5093dd05
grupoevol-creator <gentegestao.evolucao@gmail.com>
2026-07-10 09:52:22 -0300

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-// EvolPeople - Service Worker (sem cache: sempre mostra a versao nova)
-self.addEventListener("install", () => self.skipWaiting());
-self.addEventListener("activate", (e) => e.waitUntil(self.clients.claim()));
-self.addEventListener("fetch", (e) => e.respondWith(fetch(e.request)));
